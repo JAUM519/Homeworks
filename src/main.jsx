d2b7547e0d10be6099e2a6b2a9380a5f875f803e
JAUM519 <jorgeamedinau@gmail.com>
2025-10-29 16:23:06 -0500

    Challenge 4

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { ComponentApp } from './Challenge 4'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    < ComponentApp />
   </React.StrictMode>,
 )
